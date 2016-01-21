dae239b0d9654d172ac9a3752c681030f9f43217
gubbatuba <subhash_gubba@yahoo.com>
2016-01-21 12:30:49 -0500

    the popo

--- blink/blink.c
+++ blink/blink.c
@@ -4,22 +4,31 @@
 #include "ui.h"
 #include "timer.h"
 
+int a[] = {10,100,1000,10000, 1000, 100};
+// int i,*p;
+
 int16_t main(void) {
     init_clock();
     init_ui();
     init_timer();
 
     led_on(&led1);
-    timer_setPeriod(&timer2, 0.5);
+    //led_on(&led3);
+    timer_setPeriod(&timer2, 0.05);
     timer_start(&timer2);
-
+    int counter = 0;
     while (1) {
         if (timer_flag(&timer2)) {
             timer_lower(&timer2);
             led_toggle(&led1);
+            led_toggle(&led3);
+            // timer_setPeriod(&timer2, 0.5);
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
+        if (timer_flag(&timer2) && ) {
+        	timer_lower(&timer2);
+
+        }
+        //led_write(&led2, !sw_read(&sw2));
+        //led_write(&led3, !sw_read(&sw3));
     }
 }
-
