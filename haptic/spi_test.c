18321c15d5cdf7345d7a964fd9583acc5fabf0d0
RyanEggert <ryan.eggert@students.olin.edu>
2016-01-30 13:03:23 -0500

    Added angle-sensor reading code. This has yet to be tested with the board, but should be able to parse a uint16_t read from the sensor into a parity bit, an error bit, and the transmitted data (unsigned char array). Includes functions for communicating via SPI with the angle sensor. Moved some initialization code into a setup() function. Moved some declarations into a new spi_test.h file.

--- haptic/spi_test.c
+++ haptic/spi_test.c
@@ -5,54 +5,94 @@
 #include "pin.h"
 #include "spi.h"
 #include "oc.h"
+#include "spi_test.h"
 
-int main(void) {
-    int test = 0;
-    test = 2 + 4; // Tests to see whether scons builds were working.
-    float spi_freq = 24000;
-    uint8_t spi_trans = 255;
-    // Pins
-    _PIN* SPI_SCK = &D[2];
-    _PIN* SPI_MOSI = &D[0];
-    _PIN* SPI_MISO = &D[1];
-    _PIN* SPI_CS = &D[3];    
+void spi_parse_data(uint16_t data, unsigned char *angle_array) {
+    unsigned char data_size = 16;  // Size of input data (in bits)
+    unsigned char this_bit;
+    unsigned char par;
+    unsigned char ef;
+    unsigned char par_count = 0;
+    int i = 0;
+    for (i; i < data_size; ++i) {
+        this_bit = data & (1 << i) ? 1:0;
+        if (i == 0) {
+            // Even parity bit
+            par = this_bit;
+        } else if (i == 1) {
+            // Error flag
+            ef = this_bit;
 
-    _PIN* PWM_I1 = &D[8];
-    _PIN* PWM_I2 = &D[7];
+        } else if (i > 1 && i > data_size) {
+            // Transmitted data [14 bits]
+            angle_array[i-2] = this_bit;
+            if (this_bit == 1)
+                par_count++;
+        }
+    }
+    // Evaluate transmission
+    if (par != par_count) {
+        printf("%s\n", "ERR: Angle measurement parity error.");
+    }
+    if (ef == 1) {
+        printf("%s\n", "ERR: Error flag thrown by AS5048 angle sensor.");
+    }
+}
 
-    float pwm_freq = 500; // ~245Hz is minimum
-    uint16_t pwm_duty = 32768;
+void spi_send_READ(_SPI *self) {
+    spi_transfer(self, spi_READ1);
+    spi_transfer(self, spi_READ2);
+}
+
+uint16_t spi_send_NOP_read_data(_SPI *self) {
+    uint8_t msb = spi_transfer(self, spi_NOP12);
+    uint8_t lsb = spi_transfer(self, spi_NOP12);
+    return (uint16_t)((msb << 8) | lsb);
+}
+
+void read_angle_sensor(unsigned char *angle_array) {
+    pin_clear(SPI_CS);  // Assert CS LOW
+    spi_send_READ(spi_inst);
+    pin_set(SPI_CS);  // Assert CS HIGH
+    pin_clear(SPI_CS);  // Reassert CS LOW
+    // Retrieve raw angle data
+    uint16_t raw_angle = spi_send_NOP_read_data(spi_inst);
+    spi_parse_data(raw_angle, angle_array);
+}
+
+void setup(void) {
+    // Initialize PIC24 modules.
     init_clock();
     init_ui();
     init_timer();
     init_pin();
     init_oc();
     init_spi();
-    uint8_t spi_res = 0;
 
+    // Configure single SPI comms. system
+    pin_digitalOut(SPI_CS);
+    spi_open(spi_inst, SPI_MISO, SPI_MOSI, SPI_SCK, spi_freq);
+
+    // Configure & start timers used.
     timer_setPeriod(&timer1, 1);
-    timer_setPeriod(&timer2, 0.30);
+    timer_setPeriod(&timer2, 0.30);  // Timer for LED operation/status blink
     timer_start(&timer1);
     timer_start(&timer2);
-    // _PIN doug = D[4];
-    // spi1;
-    // spi_open(&spi1, &D[1], &D[0], &D[2], spi_freq); //24000 is chosen fairly arbitrarily. Revise later
-    // spi_transfer(&spi1, spi_trans); // Does angular encoder datasheet suggest 16-bit data returns? p.15
-   
+
+    // Configure single PWM signal
     oc_pwm(&oc1, PWM_I1, NULL, pwm_freq, pwm_duty);
-    while (1){
+}
+
+int main(void) {
+    setup();
+    unsigned char angle_array[14];  // LSB will be angle_array[0]
+
+    while (1) {
         if (timer_flag(&timer2)) {
+            // Blink green light to show normal operation.
             timer_lower(&timer2);
             led_toggle(&led2);
         }
-
-        if (timer_flag(&timer1)) {
-            timer_lower(&timer1);
-            led_toggle(&led1);
-            pwm_duty = pwm_duty/2;
-            pin_write(PWM_I1, pwm_duty);
-        }
-
-        // Main event loop
+        read_angle_sensor(angle_array);  // Updates angle_array
     }
 }
---
+++ haptic/spi_test.h
@@ -0,0 +1,23 @@
+// SPI Configuration
+uint8_t spi_NOP12 = 0x00;
+/* SPI READ command:
+    PAR = 1;
+    RWn = 1;
+    Address = 0x3FFF = b1111111111111;
+    Read command = 0xFFFF = b111111111111111;
+*/
+uint8_t spi_READ1 = 0xFF;   // First half of READ transmission [MSB]
+uint8_t spi_READ2 = 0xFF;   // Second half of READ transmission [LSB]
+float spi_freq = 24000;  // 24000 is chosen fairly arbitrarily. Revise later
+_SPI* spi_inst = &spi1;  // SPI instance (address of)
+
+_PIN* SPI_SCK = &D[2];
+_PIN* SPI_MOSI = &D[0];
+_PIN* SPI_MISO = &D[1];
+_PIN* SPI_CS = &D[3];
+
+// PWM Configuration
+float pwm_freq = 500;       // ~245Hz is minimum
+uint16_t pwm_duty = 0;      // Start with no commanded motor motion
+_PIN* PWM_I1 = &D[8];       // Input 1 to motor driver chip
+_PIN* PWM_I2 = &D[7];       // Input 2 to motor driver chip
